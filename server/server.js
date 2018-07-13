e8d08d2b3e1fc43ec2b3174ff3a53bbadd043f19
Alejo Coy <alejocoy17a@gmail.com>
2018-07-13 11:36:32 -0600

    views added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const _= require ('lodash');
 var express = require('express')
 var bodyParser= require('body-parser')
 var {ObjectID} = require('mongodb')
+const hbs = require('hbs');
+const methodOverride = require("method-override");
 
 
 var {mongoose} = require('./db/mongoose')
@@ -12,14 +14,25 @@ var {User} = require('./models/user')
 var port = process.env.PORT 
 
 var app = express();
-app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+'/views/partials')
+// app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+hbs.registerHelper('status',(status)=>{
+    if(status === true){
+        return 
+    }
+    
+});
 
 app.post('/todos',(req,res)=>{
     var todo = new Todo ({
         text: req.body.text
     });
     todo.save().then((todo)=>{
-        res.send(todo);
+        res.redirect("/todos");
     },(e)=>{
         res.status(400).send(e);
     })
@@ -27,7 +40,10 @@ app.post('/todos',(req,res)=>{
 
 app.get('/todos',(req,res)=> {
     Todo.find().then((todos)=>{
-        res.send({todos})
+        
+         res.render('todos.hbs',{todos})
+        //res.send({todos})
+        //  res.send({todos})
     },(e)=>{
         res.status(400).send(e)
     });
@@ -59,13 +75,31 @@ app.delete('/todos/:id',(req,res)=>{
         if(!todo){
             res.status(404).send('Todo Not found')
         }
-        res.status(200).send({todo});
+        //res.status(200).send({todo})
+        res.status(200).redirect('/todos');
     }).catch((e)=>res.status(400).send())
 });
 
 //update /todos/id Route
-app.patch('/todos/:id',(req,res)=>{
+//EDIT
+app.get('/todos/:id/edit',(req,res)=>{
     var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('Id Not Valid')
+    }
+    Todo.findById(id).then((todo)=>{
+    if(!todo){
+        res.status(404).send('Todo Not found')
+    }
+    res.render('edit.hbs',{todo});
+    });
+});
+
+//update
+
+app.put('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    console.log(req.body.text)
     var body = _.pick(req.body,['text','completed'])
     if(!ObjectID.isValid(id)){
         return res.status(404).send('Id Not Valid')
@@ -76,12 +110,13 @@ app.patch('/todos/:id',(req,res)=>{
         body.completed = false;
         body.completedAt = null;
     }
+  
 
     Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
         if(!todo){
             return res.status(404).send()
         }
-        res.status(200).send({todo})
+        res.status(200).redirect('/todos')
     }).catch((e)=>{
         res.status(400).send()
     })
